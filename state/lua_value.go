f035f7c19be0d1fecb14ae85ee0cb0bf43e1e85c
quguiyou <quguiyou1076@qq.com>
2023-04-06 22:05:13 +0800

    添加若干栈相关API

---
+++ state/lua_value.go
@@ -0,0 +1,20 @@
+package state
+
+import . "lua_go/api"
+
+type luaValue interface{}
+
+func typeOf(val luaValue) LuaType {
+	switch val.(type) {
+	case nil:
+		return LUA_TNIL
+	case bool:
+		return LUA_TBOOLEAN
+	case int64, float64:
+		return LUA_TNUMBER
+	case string:
+		return LUA_TSTRING
+	default:
+		panic("todo!")
+	}
+}
