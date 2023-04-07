2c2f0efd29239164d492aa0254e644f2ed9a8ce9
quguiyou <quguiyou1076@qq.com>
2023-04-07 19:39:40 +0800

    实现table

--- api/lua_state.go
+++ api/lua_state.go
@@ -44,4 +44,14 @@ type LuaState interface {
 	Compare(idx1, idx2 int, op CompareOp) bool
 	Concat(n int)
 	Len(idx int)
+	/* get functions (Lua -> stack) */
+	NewTable()
+	CreateTable(nArr, nRec int)
+	GetTable(idx int) LuaType
+	GetField(idx int, k string) LuaType
+	GetI(idx int, i int64) LuaType
+	/* set functions (stack -> Lua) */
+	SetTable(idx int)
+	SetField(idx int, k string)
+	SetI(idx int, i int64)
 }
